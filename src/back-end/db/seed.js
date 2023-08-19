885604ca001d419f7b97b17b0951d765a7898415
jordansaker <jordan.saker@ymail.com>
2023-08-19 18:27:53 +1000

    created users array

--- src/back-end/db/seed.js
+++ src/back-end/db/seed.js
@@ -1,5 +1,6 @@
 import LogModel from "../models/Log.js"
 import UserModel from "../models/User.js"
+import VehicleModel from "../models/Vehicle.js"
 import dotenv from 'dotenv'
 import { dbClose, dbConnect } from "./db.js"
 
@@ -11,17 +12,33 @@ dbConnect()
 await UserModel.deleteMany().then(() => console.log("Seed: users deleted"))
 await LogModel.deleteMany().then(() => console.log("Seed: logs deleted"))
 
-const user = await UserModel.create({
-    name: 'Jordan',
+
+const users = [
+  {
+    name: 'admin',
     username_id: 1111,
     password: 'test password',
     isAdmin: true
-})
+  },
+  {
+    name: 'John Smith',
+    username_id: 10001,
+    password: 'johnSmith',
+  },
+  {
+    name: 'Dom Torretto',
+    username_id: 10002,
+    password: 'FamilyIsTheBestFam',
+  }
+]
 
-const log = await LogModel.create({
+
+const log = [
+  {
     current_odo: 141000,
     fuel_added: 54,
-    user_id: user
-})
+    user_id: users[1]
+  }
+]
 
 dbClose()
