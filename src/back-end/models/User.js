50d4bbd9a425568e235bb9565a214ac7d2982998
jordansaker <jordan.saker@ymail.com>
2023-08-20 20:08:38 +1000

    completed vehicle and log seeding

--- src/back-end/models/User.js
+++ src/back-end/models/User.js
@@ -4,8 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true},
     username_id: { type: Number, cast: false, required: true},
     password: { type: String, required: true},
-    isAdmin: { type: Boolean, cast: false, required: true, default: false},
-    log_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Log' }
+    isAdmin: { type: Boolean, cast: false, required: true, default: false}
 })
 
 const UserModel = mongoose.model('User', UserSchema)
