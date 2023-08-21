c70f752b19acb066b982655a28a97abc8402d146
jordansaker <jordan.saker@ymail.com>
2023-08-21 17:08:15 +1000

    second analytical route

--- src/back-end/routes/analytical_routes.js
+++ src/back-end/routes/analytical_routes.js
@@ -1,5 +1,7 @@
 import LogModel from "../models/Log.js"
 import { Router } from "express"
+import VehicleModel from "../models/Vehicle.js"
+import mongoose from "mongoose"
 
 const router = Router()
 
@@ -15,14 +17,50 @@ router.get('/:start_date_year/:start_date_month/:start_date_day/to/:end_date_yea
       { $group: { 
           _id: { 'vehicle': '$vehicle_id' },
           totalFuel: { $sum: '$fuel_added' },
-          totalLogsRecorded: { $sum: 1 }
+          odoMin: { $min: '$current_odo' },
+          odoMax: { $max: '$current_odo' },
+          totalLogsRecorded: { $sum: 1 },
         },
       },
+      {
+        $addFields: {
+          totalDistance: { $subtract: [ '$odoMax', '$odoMin' ] }
+        }
+      },
+      // hide fields
+      { $unset: [ "odoMin", "odoMax"] },
+      // populate a vehicle field using the _id.vehicle
+      { "$lookup": {
+          "from": VehicleModel.collection.name,
+          "localField": "_id.vehicle",
+          "foreignField": "_id",
+          "as": "vehicle"
+        }
+      },
     ])
+    console.log(await LogModel.find({ date: { $lte: end_date, $gte: start_date } }))
     res.send(reportAggregate)
   } catch (err) {
     res.status(500).send({ error: err.message })
   }
+})
+
+// employer dashboard graph
+router.get('/graph/:vehicle_id/distance/', async (req, res) => {
+  const idToSearch = new mongoose.Types.ObjectId(req.params.vehicle_id)
+  const graphAggregate = await LogModel.aggregate([
+    { $match: { vehicle_id: idToSearch } },
+  ])
+  const distanceTravelledPerFill = []
+  for (let i = 0; i < graphAggregate.length; i++) {
+    if (i < graphAggregate.length - 1) {
+      distanceTravelledPerFill.push(graphAggregate[i + 1].current_odo - graphAggregate[i].current_odo)
+    }
+  }
+  res.send({
+    distanceTravelledPerFill,
+    graphAggregate
+  })
 })
 
 export default router
