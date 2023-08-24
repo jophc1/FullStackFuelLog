f619f07a0172f507481096564560c15f6503f157
jordansaker <jordan.saker@ymail.com>
2023-08-24 22:56:03 +1000

    added validation to user schema

--- src/back-end/models/User.js
+++ src/back-end/models/User.js
@@ -4,10 +4,18 @@ function usernameIdValidate (value) {
   return value > 10000
 }
 
+function nameValidation (val) {
+  return /^[A-Z][a-z]+\s[A-Z][a-z]+$/.test(val)
+}
+
+function passwordValidation (val) {
+  return /(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*])(?=.{8,})/.test(val)
+}
+
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, validate: nameValidation },
   username_id: { type: Number, required: true, unique: true, validate: usernameIdValidate },
-  password: { type: String, required: true },
+  password: { type: String, required: true, validate: passwordValidation },
   isAdmin: { type: Boolean, required: true, default: false }
 })
 
