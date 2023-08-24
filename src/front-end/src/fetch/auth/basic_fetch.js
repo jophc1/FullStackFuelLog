67d0f0891b913ff55e0446250a2d660096f84a9c
jordansaker <jordan.saker@ymail.com>
2023-08-24 12:36:44 +1000

    updated fetch options

--- src/front-end/src/fetch/auth/basic_fetch.js
+++ src/front-end/src/fetch/auth/basic_fetch.js
@@ -4,7 +4,8 @@ const basicAuthFetch = async (username, password) => {
   try {
     const res = await fetch(`${API_URL}/auth/login`, {
       headers: {
-        'Authorization': `Basic ${btoa(`${username}:${password}`)}`
+        'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
+        credentials: 'include'
       }
     })
     const returnedData = await res.json()
