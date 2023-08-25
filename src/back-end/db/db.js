765d8f2b9c10a9ecec34db133a7d4721bb1751f2
jordansaker <jordan.saker@ymail.com>
2023-08-25 20:09:17 +1000

    updated and changed env to prod, added mongodb URL

--- src/back-end/db/db.js
+++ src/back-end/db/db.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose'
 
 async function dbConnect () {
   try {
-    await mongoose.connect(`${process.env.LOCAL_DB_URL}/fuel_log`)
+    if (process.env.ENV === 'prod') {
+      await mongoose.connect(`${process.env.ATLAS_DB_URL}`)
+      console.log('PRODUCTION: Mongoose connected')
+    } else {
+      await mongoose.connect(`${process.env.LOCAL_DB_URL}/fuel_log`)
+    }
     // console.log('Mongoose connected')
   } catch (err) {
     console.log({ error: err.message })
