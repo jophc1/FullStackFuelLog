c519ac426cb3ce56cd4ef119bbc7841ee388a21f
jordansaker <jordan.saker@ymail.com>
2023-08-25 20:51:17 +1000

    updated env.js

--- src/front-end/src/env.js
+++ src/front-end/src/env.js
@@ -1,7 +1,7 @@
 // --- SET ENV VARIABLES --- //
 const ENV = import.meta.env.MODE
 let API_URL
-ENV === 'production' ? API_URL = import.meta.env.VITE_API_URL : API_URL = import.meta.env.VITE_API_LOCAL_URL
+ENV === 'production' ? API_URL = import.meta.env.VITE_API_URL : API_URL = import.meta.env.VITE_API_URL
 // ------------------------ //
 
 export default API_URL
