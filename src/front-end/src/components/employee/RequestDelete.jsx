d3c7237e41665ab1aa0293d6576a09b14ccb5ff6
jordansaker <jordan.saker@ymail.com>
2023-08-25 13:13:06 +1000

    completed request delete component

--- src/front-end/src/components/employee/RequestDelete.jsx
+++ src/front-end/src/components/employee/RequestDelete.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Card from '../styled/ProfileCard'
+import CompanyButton from '../styled/CompanyButton'
 
 const RequestDelete = () => {
-  return (
-    <div>RequestDelete</div>
-  )
+  return <>
+    <Card>
+      <h4>Log entry added successfully!</h4>
+      <p>If you made an error in the entry, you can request a delete:</p>
+      <CompanyButton>Request delete</CompanyButton>
+    </Card>
+  </>
 }
 
 export default RequestDelete
