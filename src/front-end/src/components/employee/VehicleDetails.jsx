1224d14a2271389c09b32bd6475ba23dab15b660
jordansaker <jordan.saker@ymail.com>
2023-08-25 19:00:10 +1000

    updated vehicleDetails with row component

--- src/front-end/src/components/employee/VehicleDetails.jsx
+++ src/front-end/src/components/employee/VehicleDetails.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import Card from '../styled/ProfileCard'
 import { FuelLogContext } from '../../context.js'
 import placeHolderImage from '../../assets/no-image.png'
+import Row from '../styled/Row'
 
 const VehicleDetails = ( {} ) => {
 
@@ -17,33 +18,35 @@ const VehicleDetails = ( {} ) => {
 
   return (
     <Card>
-      <div>
-        <table>
-          <tbody>
-          <tr>
-            <td>Asset ID:</td>
-            <td>{currentVehicle && currentVehicle.asset_id}</td>
-          </tr>
-          <tr>
-            <td>Reg No:</td>
-            <td>{currentVehicle && currentVehicle.registration}</td>
-          </tr>
-          <tr>
-            <td>Make:</td>
-            <td>{currentVehicle && currentVehicle.make}</td>
-          </tr>
-          <tr>
-            <td>Model:</td>
-            <td>{currentVehicle && currentVehicle.model}</td>
-          </tr>
-          <tr>
-            <td>Year:</td>
-            <td>{currentVehicle && currentVehicle.year}</td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-      <div>{!isVehicleImageReady && !currentVehicle ? <img src={placeHolderImage} />  : <img src={currentVehicle.vehicleImage_URL} />}</div>
+      <Row>
+        <div>
+          <table>
+            <tbody>
+            <tr>
+              <td>Asset ID:</td>
+              <td>{!isVehicleImageReady && !currentVehicle ? <></> : currentVehicle && currentVehicle.asset_id}</td>
+            </tr>
+            <tr>
+              <td>Reg No:</td>
+              <td>{!isVehicleImageReady && !currentVehicle ? <></> : currentVehicle && currentVehicle.registration}</td>
+            </tr>
+            <tr>
+              <td>Make:</td>
+              <td>{!isVehicleImageReady && !currentVehicle ? <></> : currentVehicle && currentVehicle.make}</td>
+            </tr>
+            <tr>
+              <td>Model:</td>
+              <td>{!isVehicleImageReady && !currentVehicle ? <></> : currentVehicle && currentVehicle.model}</td>
+            </tr>
+            <tr>
+              <td>Year:</td>
+              <td>{!isVehicleImageReady && !currentVehicle ? <></> : currentVehicle && currentVehicle.year}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className='vehicleImg' >{!isVehicleImageReady && !currentVehicle ? <img src={placeHolderImage} />  : <img src={currentVehicle.vehicleImage_URL} />}</div>
+      </Row> 
     </Card>
   )
 }
