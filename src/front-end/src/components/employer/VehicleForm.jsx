8253f57b12723d387b04c817fee4e346410ae299
jordansaker <jordan.saker@ymail.com>
2023-08-26 13:30:41 +1000

    added states to vehicle form

--- src/front-end/src/components/employer/VehicleForm.jsx
+++ src/front-end/src/components/employer/VehicleForm.jsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react'
 
 const VehicleForm = () => {
 
+  const [make, setMake] = useState('')
+  const [model, setModel] = useState('')
+  const [year, setYear] = useState('')
+  const [assetId, setAssetId] = useState('')
+  const [rego, setRego] = useState('')
   const [selectedFile, setSelectedFile] = useState(null)
 
   const handleSubmit = event => {
@@ -11,21 +16,21 @@ const VehicleForm = () => {
 
   return <>
     <h3>Add Vehicle</h3>
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Car Make</label>
-        <input type="text" />
+        <input type="text" value={make} onChange={event => setMake(event.target.value)} />
         <label>Model</label>
-        <input type="text" />
+        <input type="text" value={model} onChange={event => setModel(event.target.value)} />
         <label>Year</label>
-        <input type="text" />
+        <input type="text" value={year} onChange={event => setYear(event.target.value)} />
         <label>Asset ID</label>
-        <input type="text" />
+        <input type="text" value={assetId} onChange={event => setAssetId(event.target.value)} />
         <label>Registration</label>
-        <input type="text" />
+        <input type="text" value={rego} onChange={event => setRego(event.target.value)} />
       </div>
       <div>
-        <input type="file" value={selectedFile} onChange={event => console.log(event)} />
+        <input type="file" value={selectedFile} onChange={event => setSelectedFile(event.target.value)} />
       </div>
       <input type="submit" value="Submit" />
     </form>
