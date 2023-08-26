d5b513fd01193426bba46ddc7f9fda4a06f20974
jordansaker <jordan.saker@ymail.com>
2023-08-27 07:30:38 +1000

    created Modal files

---
+++ src/front-end/src/components/ModalField.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PopUpModalFields = () => {
+  return (
+    <div>PopUpModalFields</div>
+  )
+}
+
+export default PopUpModalFields
---
+++ src/front-end/src/components/ModalText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PopUpModelsText = () => {
+  return (
+    <div>PopUpModelsText</div>
+  )
+}
+
+export default PopUpModelsText
