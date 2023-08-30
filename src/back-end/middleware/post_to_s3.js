413b88898199358b1db8e49e742574d71fbea5f2
jophc1 <jphcairns@gmail.com>
2023-08-30 11:56:21 +1000

    modified image format in s3 middleware

--- src/back-end/middleware/post_to_s3.js
+++ src/back-end/middleware/post_to_s3.js
@@ -14,7 +14,7 @@ async function postToS3 (req, res, next) {
     }
 
     if (!req.testRoute && checkImage ) { // testRoute is for test routes so a image isnt created with a post vehicle, sorry jordan if this breaks your images
-      let KEY = `${req.body.asset_id}.png`
+      let KEY = `${req.body.asset_id}`
 
       req.key = KEY
       const client = new S3Client({
