14242ce6888beb8c20cee956bbaf092df5dc14e6
jordansaker <jordan.saker@ymail.com>
2023-08-31 17:20:05 +1000

    updated cors

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -13,16 +13,18 @@ import helmet from 'helmet'
 
 // Cors options and whitelisting
 // TODO: NEED TO ADD WHITELIST FOR DEPLOYED REACT APP SERVER IP ADDRESS
-const whitelist = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://fuel-log.onrender.com/'] 
+const whitelist = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://fuel-log.onrender.com'] 
 const corsOptions = {
   credentials: true,
+  // origin: '*',
   origin: function(origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1){
        callback(null, true)
     } else {
       callback(new Error('Blocked by CORS'))
     }
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS']
 }
 
 dotenv.config()
