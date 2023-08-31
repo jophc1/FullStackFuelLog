0365b1e29056f2f2d6c69784b39a244cf4587f4a
jordansaker <jordan.saker@ymail.com>
2023-08-31 11:02:27 +1000

    table report styled

--- src/front-end/src/components/Pagination.jsx
+++ src/front-end/src/components/Pagination.jsx
@@ -54,7 +54,7 @@ const Pagination = ({ currentPage = 1, totalPages, hasPrevPage, hasNextPage, nex
    <PaginationButtons>
       {hidePrevPage && <li key={prevPage} onClick={handleDisplayPreviousPages}>&hellip;</li>}
     {pagesToDisplay.slice(startIndex, endIndex).map(page => (
-        <li key={page} value={page} onClick={handlePageClick}>{page}</li>
+        <li key={page} value={page} onClick={handlePageClick} className='pageNumber'>{page}</li>
     ))}
       {hideNextPage &&  <li key={nextPage} onClick={handleDisplayNextPages}>&hellip;</li>}
     </PaginationButtons>
--- src/front-end/src/components/styled/PaginationButtons.jsx
+++ src/front-end/src/components/styled/PaginationButtons.jsx
@@ -4,7 +4,7 @@ const PaginationButtons = styled.ul`
   display: flex;
   list-style-type: none;
   justify-content: center;
-
+  margin-top: 1rem;
 `
 
 export default PaginationButtons
