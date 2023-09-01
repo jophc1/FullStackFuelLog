8e23562ca61c49aebed5157a302655e17a1c8aa4
jordansaker <jordan.saker@ymail.com>
2023-09-01 14:21:46 +1000

    fuel validation

--- src/back-end/models/Log.js
+++ src/back-end/models/Log.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose'
 import mongoosePaginate from 'mongoose-paginate-v2'
 
+function fuelAddedValidation (val) {
+  return val > 0
+}
+
 const LogSchema = new mongoose.Schema({
   date: { type: Date, cast: false, default: new Date() },
   current_odo: { type: Number, required: true, cast: false },
-  fuel_added: { type: Number, required: true, cast: false },
+  fuel_added: { type: Number, required: true, cast: false, validate: fuelAddedValidation },
   vehicle_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
