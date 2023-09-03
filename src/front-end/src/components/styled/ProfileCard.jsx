b6f9619b3efeffdc1b4f9b4fd04fa6bf1406b84f
jordansaker <110700134+jordansaker@users.noreply.github.com>
2023-09-03 21:38:34 +1000

    Delete src/front-end directory

--- src/front-end/src/components/styled/ProfileCard.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const Card = styled.div`
-  border: 3px solid #d9d9d9;
-  border-radius: 5px;
-`
-export default Card
--- src/front-end/src/context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react'
-
-const FuelLogContext = createContext()
-const EmployerContext = createContext()
-
-export { FuelLogContext, EmployerContext }
